1917d276a284a2fa353d17ef89ada1ec2a38de1c
Ryan Kristoffer <ryank@peplink.com>
2020-01-15 16:05:04 +0800

    added getting of input and changing the value from store

--- src/js/TodoList.js
+++ src/js/TodoList.js
@@ -1,9 +1,21 @@
 import React from "react"
-import {observer} from "mobx-react"
+import { observer } from "mobx-react"
 
 @observer
 export default class TodoList extends React.Component {
+  inputFilter(e) {
+    this.props.store.filter = e.target.value
+  }
+
   render() {
-    return <h1>{this.props.store.todos[0]}</h1>
+    const { todos, filter } = this.props.store
+    const todoLis = todos.map(todo => (
+      <li>{todo}</li>
+    ))
+    return <div>
+      <h1>{filter}</h1>
+      <input className="filter" value={filter} onChange={this.inputFilter.bind(this)} />
+      <ul>{todoLis}</ul>
+    </div>
   }
 }
