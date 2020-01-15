ecddfd3fc0362535a879a611ae92259d01a30e1c
Ryan Kristoffer <ryank@peplink.com>
2020-01-15 15:55:41 +0800

    pass store to todolist, and load to render()

--- src/js/TodoList.js
+++ src/js/TodoList.js
@@ -1,7 +1,9 @@
 import React from "react"
+import {observer} from "mobx-react"
 
+@observer
 export default class TodoList extends React.Component {
   render() {
-    return <h1>MobX</h1>
+    return <h1>{this.props.store.todos[0]}</h1>
   }
 }
